430cfd23e0c392e8c64bec9c8c8ed359e866300e
David Wilson <dw@botanicus.net>
2013-04-16 17:30:50 +0300

    Absolute imports.

--- mongotron/Collection.py
+++ mongotron/Collection.py
@@ -1,5 +1,7 @@
+
+from __future__ import absolute_import
 from pymongo.collection import Collection as PymongoCollection
-from Cursor import Cursor
+from .Cursor import Cursor
 
 class Collection(PymongoCollection):
 
