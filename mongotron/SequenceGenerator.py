ba08335840b3b27af7ae08d400792b5f5cc033dd
David Wilson <dw@botanicus.net>
2013-05-26 14:17:13 +0300

    Let SequenceGenerator work from classmethods.

--- mongotron/SequenceGenerator.py
+++ mongotron/SequenceGenerator.py
@@ -2,12 +2,15 @@
 from __future__ import absolute_import
 from .ConnectionManager import GetConnectionManager
 
+
 class SequenceGenerator(object):
     
     @classmethod
-    def get_next_index(cls, object, database_name, collection_name, connection_name=None):
-        name = object.__class__.__name__
-        
+    def get_next_index(cls, seq_name, database_name, collection_name, connection_name=None):
+        # TODO: remove me after a few releases.
+        if not isinstance(seq_name, basestring):
+            seq_name = seq_name.__class_.__name__
+
         connection = GetConnectionManager().get_connection(connection_name, True)
         
         collection = connection[database_name][collection_name]
@@ -15,7 +18,7 @@ class SequenceGenerator(object):
         # we *DO* ensure an index here, it might be important
         collection.ensure_index("name")
 
-        new_id = collection.find_and_modify(query={"name":name},
+        new_id = collection.find_and_modify(query={"name":seq_name},
                                             update={"$inc":{"seq":long(1)}},
                                             new=True,
                                             upsert=True).get("seq")
